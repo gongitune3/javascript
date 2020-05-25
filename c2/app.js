d342dfdebdbc5bbd194b08bec606eee3edebfbb2
gongitune14 <inoseshouhei@inoseshouheinoMacBook-Air.local>
2020-05-25 22:37:05 +0900

    [Finish] 6-2

--- c2/app.js
+++ c2/app.js
@@ -1,5 +1,24 @@
-var hello = 'Hello World';
+var max = 100;
+var num = 1;
+var count = 0;
+ 
+while(num < max){
+    num = num * 2;
+    count = count + 1;
+}
+ 
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
 
-alert(hello);
+var i;
+var num = 0;
+ 
+for(i = 1; i < 11; i++){
+    num = num + i;
+}
+ 
+alert('1から10まで足し算した結果は' + num + 'です');
 
-var 
+alert(1 + 3);
+alert(10 - 6);
+alert(2 * 2);
+alert(32 / 8);
